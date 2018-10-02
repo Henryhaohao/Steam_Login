e46cd42ed4607e58750ab7b257fac0ab59c668ba
Henryhaohao <1073064953@qq.com>
2018-10-02 17:12:05 +0800

    Steam_Login:star:

---
+++ README.md
@@ -0,0 +1,31 @@
+Python实现Steam游戏平台的模拟登录 ![enter image description here](Pic/logo.png)
+===========================
+![](https://img.shields.io/badge/Python-3.6.3-green.svg) ![](https://img.shields.io/badge/requests-2.18.4-green.svg) ![](https://img.shields.io/badge/PyExecJS-1.5.1-green.svg) 
+### Steam游戏平台 - https://store.steampowered.com/
+|Author|:sunglasses:Henryhaohao:sunglasses:|
+|---|---
+|Email|:hearts:1073064953@qq.com:hearts:
+
+      
+****
+## :dolphin:声明
+软件均仅用于学习交流，请勿用于任何商业用途！感谢大家！
+## :dolphin:介绍
+该项目为[Steam游戏平台](https://store.steampowered.com/)的模拟登录
+- 爬虫文件:Spiders目录下的steam_login.py
+- password密码解密文件:Spiders目录下的rsa.js
+## :dolphin:运行环境
+Version: Python3
+## :dolphin:安装依赖库
+```
+pip3 install -r requirements.txt
+```
+## :dolphin:**运行截图**
+> - **Steam官网 - https://store.steampowered.com/**<br><br>
+![enter image description here](Pic/steam.png)
+
+> - **运行过程**<br><br>
+![enter image description here](Pic/run.png)
+
+
+
---
+++ Spiders/steam_login.py
@@ -0,0 +1,100 @@
+# !/user/bin/env python
+# -*- coding:utf-8 -*- 
+# time: 2018/9/6--19:24
+__author__ = 'Henry'
+
+'''
+Steam登录 (RSA加密)
+网址:https://store.steampowered.com/login/
+'''
+
+import requests,execjs,time,re
+import requests.cookies
+
+
+def steam_login():
+    req = requests.session()
+    headers = {
+        'Referer':'https://store.steampowered.com/login/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    }
+    # 获取RSA公钥的了两个部分(mod 模数,exp 指数)
+    url = 'https://store.steampowered.com/login/getrsakey/'
+    data = {
+    'donotcache':str(int(time.time()*1000)),
+    'username':user
+    }
+    html = req.post(url,data=data,headers=headers).json()
+    print(html)
+    pub_mod = html.get('publickey_mod')
+    pub_exp = html.get('publickey_exp')
+    timestamp = html.get('timestamp')
+    # 加密密码
+    with open('rsa.js',encoding='utf-8') as f:
+        jsdata = f.read()
+    passencrypt = execjs.compile(jsdata).call('getpwd',password,pub_mod,pub_exp)
+    print(passencrypt)
+    # 登录
+    url = 'https://store.steampowered.com/login/dologin/'
+    data = {
+        'donotcache':str(int(time.time()*1000)),
+        'username':user,
+        'password':passencrypt,
+        'twofactorcode':'',
+        'emailauth':'',
+        'loginfriendlyname':'',
+        'captchagid':'-1',
+        'captcha_text':'',
+        'emailsteamid':'',
+        'rsatimestamp':timestamp,
+        'remember_login':'false',
+    }
+    html = req.post(url,data=data,headers=headers).json()
+    print(html)
+    if html.get('emailauth_needed') == True:
+        print('登录需要您的邮箱验证码...')
+        emailid = html.get('emailsteamid')
+        email = input('请输入您的邮箱验证码:')
+        # 带上验证码重新登录
+        data['emailauth'] = email
+        data['emailsteamid'] = emailid
+        html = req.post(url, data=data, headers=headers).json()
+        print(html)
+        if html.get('login_complete') == True and html.get('success') == True:
+            print('正在提交登录...')
+            # 登录到个人中心
+            # 方法一: 利用requests.cookies.update()函数添加新的cookie字段
+            # 获取token,webcookie新的cookie字段
+            steamid = html.get('transfer_parameters').get('steamid')
+            token = html.get('transfer_parameters').get('token_secure')
+            webcookie = html.get('transfer_parameters').get('webcookie')
+            cookie = requests.cookies.RequestsCookieJar()
+            cookie.set('steamLoginSecure',steamid + '%7C%7C' + token)
+            cookie.set('steamMachineAuth',webcookie)
+            req.cookies.update(cookie) # 更新cookie
+            url_store = 'https://store.steampowered.com/'
+            html = req.get(url_store, headers=headers).text
+            # print(html)
+            username = re.findall(r'data-miniprofile=".*?">(.*?)</a>', html)[0]
+            if username == user:
+                print('登录成功!用户名:' + username)
+
+            # 方法二:一直保持session,就不用更新cookie了,直接请求即可
+            # url_store = 'https://store.steampowered.com/'
+            # html = req.get(url_store,headers=headers).text
+            # print(html)
+            # username = re.findall(r'data-miniprofile=".*?">(.*?)</a>',html)[0]
+            # if username == user:
+            #     print('[登录成功!用户名:' + username +']')
+
+        else:
+            print('登录失败!请重新登录!')
+    elif html.get('success') == False and html.get('message') != '':
+        print(html.get('message'))
+        print('登录失败!请重新登录!')
+
+
+if __name__ == '__main__':
+    user = input('请输入您的steam账号:')
+    password = input('请输入您的steam密码:')
+    steam_login()
